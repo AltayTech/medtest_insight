23ccc390efcdac35c85c9fbd8771248b27517ddc
Reza Mojed <mojed.reza@gmail.com>
2023-11-10 19:58:33 +0300

    Add scan button to home screen

--- lib/features/home_screen/presentation/pages/home_screen.dart
+++ lib/features/home_screen/presentation/pages/home_screen.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
   static const routeName = '/HomeScreen';
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: Center(
-        child: Text('home screen'),
+        /// Add bottom to navigate to scan screen
+        child: ElevatedButton(
+          onPressed: () {},
+          child: Text('Scan Test result'),
+        ),
       ),
     );
   }
