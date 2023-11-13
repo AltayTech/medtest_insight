1aafd6f5b0c83e889f7093eeaf499a4cd2ea6f20
Reza Mojed <mojed.reza@gmail.com>
2023-11-13 19:43:31 +0300

    Initialized locator

--- lib/features/scan/data/datasources/scan_remote_data_source.dart
+++ lib/features/scan/data/datasources/scan_remote_data_source.dart
@@ -2,29 +2,29 @@ import 'package:dio/dio.dart';
 import '../../../../../core/errors/exceptions.dart';
 import '../../../../../core/params/params.dart';
 import '../models/scan_model.dart';
-
-abstract class ScanRemoteDataSource {
-  Future<ScanModel> getScan({required ScanParams scanParams});
-}
-
-class ScanRemoteDataSourceImpl implements ScanRemoteDataSource {
-  final Dio dio;
-
-  ScanRemoteDataSourceImpl({required this.dio});
-
-  @override
-  Future<ScanModel> getScan({required ScanParams scanParams}) async {
-    final response = await dio.get(
-      'https://pokeapi.co/api/v2/pokemon/',
-      queryParameters: {
-        'api_key': 'if needed',
-      },
-    );
-
-    if (response.statusCode == 200) {
-      return ScanModel.fromJson(json: response.data);
-    } else {
-      throw ServerException();
-    }
-  }
-}
+//
+// abstract class ScanRemoteDataSource {
+//   Future<ScanModel> getScan({required ScanParams scanParams});
+// }
+//
+// class ScanRemoteDataSourceImpl implements ScanRemoteDataSource {
+//   final Dio dio;
+//
+//   ScanRemoteDataSourceImpl({required this.dio});
+//
+//   @override
+//   Future<ScanModel> getScan({required ScanParams scanParams}) async {
+//     final response = await dio.get(
+//       'https://pokeapi.co/api/v2/pokemon/',
+//       queryParameters: {
+//         'api_key': 'if needed',
+//       },
+//     );
+//
+//     if (response.statusCode == 200) {
+//       return ScanModel.fromJson(json: response.data);
+//     } else {
+//       throw ServerException();
+//     }
+//   }
+// }
--- lib/locator.dart
+++ lib/locator.dart
@@ -1,19 +1,39 @@
+import 'package:data_connection_checker_tv/data_connection_checker.dart';
 import 'package:get_it/get_it.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:medtest_insight/core/connection/network_info.dart';
 import 'package:medtest_insight/features/scan/presentation/providers/scan_provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+import 'features/scan/business/repositories/scan_repository.dart';
+import 'features/scan/business/usecases/get_scan_usecase.dart';
 import 'features/scan/data/datasources/scan_local_data_source.dart';
+import 'features/scan/data/datasources/scan_storage_data_source.dart';
+import 'features/scan/data/repositories/scan_repository_impl.dart';
 
 GetIt locator = GetIt.instance;
 
 setup() async {
-  /// Providers
-  locator.registerSingleton<ScanProvider>(ScanProvider());
   final sharedPreference = await SharedPreferences.getInstance();
-  locator.registerSingleton<ScanLocalDataSourceImpl>(
+  locator.registerSingleton<ScanLocalDataSource>(
       ScanLocalDataSourceImpl(sharedPreferences: sharedPreference));
 
+  final imagePicker = ImagePicker();
+  locator.registerSingleton<ScanStorageDataSource>(
+      ScanStorageDataSourceImpl(imagePicker: imagePicker));
 
   /// Repositories
+  final networkInfo = NetworkInfoImpl(DataConnectionChecker());
+  locator.registerSingleton<ScanRepository>(ScanRepositoryImpl(
+    storageDataSource: locator(),
+    localDataSource: locator(),
+    networkInfo: networkInfo,
+  ));
 
+  /// use case
+  locator.registerSingleton<GetScanUseCase>(
+      GetScanUseCase(scanRepository: locator()));
+
+  /// statemanagement
+  locator.registerSingleton<ScanProvider>(ScanProvider());
 }
