e19d2c1ed31ba67d909ff1da7cf6c01d3de80cd2
Reza Mojed <mojed.reza@gmail.com>
2023-11-18 13:58:46 +0300

    Removed redundant code

--- lib/core/constants/constants.dart
+++ lib/core/constants/constants.dart
@@ -1,18 +1,5 @@
 String kTemplate = 'template';
 
-String kScan = 'scan';
-
-String kName = 'name';
-String kId = 'id';
-String kSprites = 'sprites';
-String kTypes = 'types';
-String kType = 'type';
-String kOther = 'other';
-String kOfficialArtwork = 'official-artwork';
-String kFrontDefault = 'front_default';
-String kFrontShiny = 'front_shiny';
-
-int maxPokemonId = 1008;
 String CPrompt =
     'This is medical test. You are a medical expert and you are expert doctor. Please analyse this data and give recommendation or suggestion. An suppose that the person who you are explaining to him is normal person not a expert. Consider that it can be any type of test'
     'such as blood test or other test. Please consider all probability for any diseases or illness such as cancer or diabetes have cold.'
