a557f5636704faf8b7db40220ddb8c49f81f8e53
Reza Mojed <mojed.reza@gmail.com>
2023-11-18 18:57:44 +0300

    Added authPage.dart Added firebase_auth package

---
+++ lib/features/athentication_feature/presentation/pages/authPage.dart
@@ -0,0 +1,23 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:medtest_insight/features/home_screen/presentation/pages/home_screen.dart';
+
+class AuthPage extends StatelessWidget {
+  const AuthPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return HomeScreen();
+          } else {
+            return HomeScreen();
+          }
+        },
+      ),
+    );
+  }
+}
